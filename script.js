495a90a41faab4c1e1cfa90c0b622b9473a29e1b
DrakeWu <52110568+DrakeWu@users.noreply.github.com>
2026-06-22 01:36:37 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -13,4 +13,28 @@ function typeWriter(text, elementId, delay = 100) {
     }
     type();
 }
-typeWriter("CO/2029 ECE Major at Temple University.", "aboutText", 50);
+typeWriter("Hello, I'm Drake, a student at Temple University", "aboutText", 50);
+
+const projects = [
+    {
+        title: "In-Class Incubator Project",
+        image: "incubator.jpg",
+        description: "A project where we designed and built a class incubator using a microcontroller, sensors, and a heating element to maintain certain temperature states."
+    },
+    {
+        title: "Posture Detector",
+        image: "posturephoto.jpg",
+        description: "Posture detector that uses sensors to provide feedback on sitting posture."
+    }
+];
+
+const projectsContainer = document.getElementById("projects");
+let currentprojects = 0;
+
+function cycleProjects() {
+    currentprojects++;
+    if (currentprojects >= projects.length) {
+        currentprojects = 0;
+    }
+    const project = projects[currentprojects];
+}
